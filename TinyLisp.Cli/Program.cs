5d288ff6a9b3a8e2ef73313572a428596147b1f2
Ade Attwood <adrian.attwood@marketdojo.com>
2025-03-09 20:46:41 +0000

    chore: create a really simple repl

--- TinyLisp.Cli/Program.cs
+++ TinyLisp.Cli/Program.cs
@@ -1,11 +1,6 @@
 ﻿using TinyLisp.Cli;
 
-var command = args.ElementAtOrDefault(0);
-if (command == null)
-{
-    throw new Exception("No command given");
-}
-
+var command = args.ElementAtOrDefault(0) ?? "repl";
 switch (command)
 {
     case "run":
@@ -13,6 +8,9 @@ switch (command)
         break;
     case "test":
         new CommandTest().Run(args.ElementAtOrDefault(1) ?? throw new Exception("Test command requires a directory"));
+        break;
+    case "repl":
+        new CommandRepl().Run();
         break;
     default:
         throw new Exception($"Invalid command '{command}'");
